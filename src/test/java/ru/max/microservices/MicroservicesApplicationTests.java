8a51e5814a8415559fb266ca519feb9d537e6d64
Maxim <mksmsmirnov@gmail.com>
2021-07-13 18:36:38 +0300

    Initial commit

---
+++ src/test/java/ru/max/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.max.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
